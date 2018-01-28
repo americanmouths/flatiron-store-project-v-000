80f90aaa012b6743035b1aed90fc10d5a537143a
americanmouths <shiverr@gmail.com>
2018-01-28 13:58:15 -0800

    Passing all cart model tests

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,21 @@ class Cart < ActiveRecord::Base
   has_many :line_items
   has_many :items, :through => :line_items
   belongs_to :user
+
+
+  def total
+    @summ = 0
+    @summ = self.items.inject(0) {|sum, item| sum + item.price}
+  end
+
+  def add_item(item)
+    line_item = LineItem.find_by(item_id: item)
+    if line_item
+      line_item.quantity += 1
+    else
+      line_item = LineItem.new(item_id: item, cart_id: self.id)
+    end
+      line_item
+  end
+
 end

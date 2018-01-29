ab9938ce890bd961e981b4e07ed365aabeb91d3f
americanmouths <shiverr@gmail.com>
2018-01-28 16:38:52 -0800

    Passing a few more specs

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
   def show
-    @category = Category.find(params[:id])
-    @items = @category.items
+    @category = Category.find_by_id(params[:id])
   end
 end
